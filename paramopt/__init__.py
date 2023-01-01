de943291c0e7783ad2f24b5c3849b82c354ff1c5
Kota AONO <k.pg.aono@gmail.com>
2023-01-01 17:50:05 +0900

    Version up

--- paramopt/__init__.py
+++ paramopt/__init__.py
@@ -1,7 +1,7 @@
 from .acquisitions import EI, UCB, BaseAcquisition
 from .extensions import (AutoHyperparameter, AutoHyperparameterRegressor,
-                         create_gif, select_images)
+                         UnduplicatedSuggestor, create_gif, select_images)
 from .optimizer import BayesianOptimizer
 from .parameter import ExplorationSpace
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
