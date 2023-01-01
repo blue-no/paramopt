48b9a361978da3ff5b257a1809e70170421a428d
Kota AONO <k.pg.aono@gmail.com>
2023-01-01 21:36:56 +0900

    Add test

--- tests/test_imple.py
+++ tests/test_imple.py
@@ -1,6 +1,7 @@
 import warnings
 
 import numpy as np
+import pytest
 
 with warnings.catch_warnings():
     warnings.simplefilter('ignore', PendingDeprecationWarning)
@@ -8,6 +9,7 @@ with warnings.catch_warnings():
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 
+from paramopt.imples import BaseImple
 from paramopt.imples.gpy import GpyImple
 from paramopt.imples.sklearn import SklearnImple
 
@@ -18,6 +20,14 @@ def _create_data():
     x = np.random.random(size=(N, 1))
     y = np.random.random(size=(N, 1))
     return (x, y)
+
+
+def test_base_imple():
+    imple = BaseImple()
+    with pytest.raises(NotImplementedError):
+        imple.fit(*_create_data())
+    with pytest.raises(NotImplementedError):
+        imple.predict(_create_data()[0])
 
 
 def test_sklearn_imple():
