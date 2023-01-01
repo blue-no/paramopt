6b1895a9958a993a70b682c31a1700a056f04e13
Kota AONO <k.pg.aono@gmail.com>
2023-01-01 11:49:20 +0900

    Initial commit

---
+++ tests/test_demo.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from paramopt import UCB, BayesianOptimizer, ExplorationSpace
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+
+
+folder = Path('tests', 'result', 'demo_2d')
+folder.mkdir(exist_ok=True)
+
+def obj_func(X):
+    return -(X[0]-5)**2-(X[1]-50)**2/50
+
+optimizer = BayesianOptimizer(
+    regressor=GaussianProcessRegressor(
+        kernel=C()*RBF(length_scale_bounds='fixed'), normalize_y=True),
+    exp_space=ExplorationSpace({
+        'distance':
+            {'values': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+             'unit': 'mm'},
+        'temperature':
+            {'values': [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+             'unit': '°C'}
+    }),
+    eval_name='quality',
+    acq_func=UCB(c=2.0),
+    obj_func=obj_func,
+    normalize_X=True)
+
+for i in range(10):
+    next_params = optimizer.suggest()
+    y = obj_func(next_params)
+    optimizer.update(X=next_params, y=y, label=i)
+    optimizer.plot_distribution(save_as=folder.joinpath(f'dist-{i}.png'))
+    optimizer.plot_transition(save_as=folder.joinpath(f'trans-{i}.png'))
+    optimizer.save_history(folder.joinpath('history.csv'))
