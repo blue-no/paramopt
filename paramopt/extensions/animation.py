46df512d633f1e1fa398a632d42a48d03eda2978
Kota AONO <k.pg.aono@gmail.com>
2023-01-01 11:12:30 +0900

    Change file save process

--- paramopt/extensions/animation.py
+++ paramopt/extensions/animation.py
@@ -1,22 +1,12 @@
-import os.path as osp
 from typing import List
 
 from natsort import natsorted
 from PIL import Image
 
 
-def unique_path(path):
-    ret = path
-    stem, ext = osp.splitext(path)
-    i = 1
-    while osp.isfile(ret):
-        ret = stem + f' ({i})' + ext
-        i += 1
-    return ret
-
-
 def create_gif(
-    img_paths: List[str],
+    fp: str,
+    img_fps: List[str],
     duration: float = 1.0,
     loop: int = 0
 ) -> None:
@@ -24,21 +14,21 @@ def create_gif(
 
     Parameters
     ----------
-    img_paths : list of strs
+    fp : str
+        Name of the output video
+    img_fps : list of strs
         List of image paths
     duration : float, optional
         Time interval between frames [s], by default 1.0.
     loop : int, optional
         Number of loop, by default 0 (infinite)
     """
-    if len(img_paths) == 0:
+    if len(img_fps) == 0:
         print('No file')
         return
-    folder = osp.dirname(img_paths[0])
-    save_path = unique_path(osp.join(folder, 'animation.gif'))
-    images = list(map(lambda file: Image.open(file), natsorted(img_paths)))
+    images = list(map(lambda file: Image.open(file), natsorted(img_fps)))
     images[0].save(
-        save_path, save_all=True, append_images=images[1:],
+        fp, save_all=True, append_images=images[1:],
         duration=float(duration)*1000, loop=int(loop))
     print('Done')
 
@@ -58,7 +48,7 @@ def select_images() -> List[str]:
     except:
         pass
     Tk().withdraw()
-    img_paths = filedialog.askopenfilenames(
+    img_fps = filedialog.askopenfilenames(
         filetypes=[
             ('PNG files', '*.png'), ('JPEG files', '*.jpg')], initialdir='.')
-    return img_paths
+    return img_fps
